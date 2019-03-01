c1435183390dc42c3e54ee35a9a0f2778e87ef6a
System Administrator <root@Owners-MacBook-Pro.local>
2019-03-01 15:22:33 +0300

    add myfunction()

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 var sound = new Audio();
 sound.src ="music/explosion.mp3";
 
-$(document).ready(function(){
-  var player1= $("#player1").val();
-  var player2= $("#player2").val();
+function myFunction() {
+  var playerOne = document.getElementById("playerOne").value;
+  document.getElementById("p1Name").innerHTML = playerOne;
+  var playerTwo = document.getElementById("playerTwo").value;
+  document.getElementById("p2Name").innerHTML = playerTwo;
+};
 
+$(document).ready(function(){
   $("#start-game").click(function(){
     event.preventDefault();
     $("#page1").fadeOut();
     $("#page2").fadeIn();
-    
-
-
-  });
+});
 
 });
