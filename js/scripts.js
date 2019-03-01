9afe69070f247125bb362e39e7aaeb6a5784cfb0
System Administrator <root@Owners-MacBook-Pro.local>
2019-03-01 13:49:59 +0300

    add fade in fade out functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ var sound = new Audio();
 sound.src ="music/explosion.mp3";
 
 $(document).ready(function(){
+  var player1= $("#player1").val();
+  var player2= $("#player2").val();
 
+  $("#start-game").click(function(){
+    event.preventDefault();
+    $("#page1").fadeOut();
+    $("#page2").fadeIn();
+    
+
+
+  });
 
 });
